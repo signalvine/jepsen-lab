2d2aef13534cebd56aaa87a29082da33e5f3b188
Brendan Tobolaski <brendan@tobolaski.com>
2015-11-17 15:10:35 -0500

    Resolve a couple of inconviences

--- control-setup.sh
+++ control-setup.sh
@@ -2,9 +2,13 @@
 set -e
 set -x
 
+# Fix permissions on the private key
+chmod 600 ~/.ssh/id_rsa
+
+# Get openjdk8
 echo 'deb http://cloudfront.debian.net/debian jessie-backports main' | sudo tee /etc/apt/sources.list.d/backports.list
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jre openjdk-8-jre-headless libjna-java git
+sudo apt-get install -y openjdk-8-jre openjdk-8-jre-headless git
 
 # Install Leiningen
 mkdir -p ~/.bin
